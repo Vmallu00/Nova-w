67bfa8341dfa54f5d922b68e2ec6d88727d624d0
𓆰𓆪 <168411239+Vmallu00@users.noreply.github.com>
2026-09-20 18:55:17 +0530

    Update bot-manager.js

--- bot-manager.js
+++ bot-manager.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const crypto = require("crypto");
-const { FakePixelBot } = require("./fakepixel-bot");
+const FakePixelBot = require('./fakepixel-bot');
 
 class BotManager {
   constructor(database) {
